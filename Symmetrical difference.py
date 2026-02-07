8095d4da5de60fe775b3b651b1008aa0ef56fa95
Brawlstars902 <aparna.rautela@gmail.com>
2026-02-08 05:15:17 +0530

    Add files via upload

---
+++ Symmetrical difference.py
@@ -0,0 +1,7 @@
+z={'Hello','monkey',20,15,27.26,'6dcr',31,1.9,'Thirty'}
+f={'hi','Hello',31,20,47,8.4,'dctecgv','hmmm','monkey'}
+
+print('\nSet A:  ',z)
+print('Set B:  ',f)
+
+print('\nSymmetrical Difference:  '+str(z.symmetric_difference(f)),'\n\n')
